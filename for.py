b1857a08116f156f827da53fb48f49369d814367
aguscarrera77 <aguscarrera77@gmail.com>
2026-04-14 16:31:36 -0300

    14/4

--- for.py
+++ for.py
@@ -61,5 +61,142 @@ perfiles={
 
 }
 
-for clave in perfiles.keys():
+for clave in perfiles.items():
     print(clave)
+
+# no solamente el for se usa para recorrer estructuras o conjuntos sino por ej:
+#Transforma datos. Genero datos nuevos.
+
+numbers=[4,5,8,10,21]
+doble=[]
+
+for n in numbers:
+    doble.append(n*2)#append agrega elementos.
+
+print(doble)
+
+#filtrar: con una condicion puedo filtar y generar nuevos conjuntos.Usando if.
+
+datos=[10,30,35,None,4]
+
+for d in datos:
+    if d is None:
+        print("Dato es invalido.")
+    
+altos=[]
+
+for a in [150,200,90,70,540]:
+    if a > 150:
+        altos.append(a)
+
+print(altos)
+
+#acumular: valor final por ej de una suma.
+
+suma=0
+
+for n in [20,4,10]:
+    suma= suma + n
+
+print(suma)
+
+# Control del flujo del for: 
+
+for numero in range(10): #range metodo usa rango.
+    if numero==7:
+        break
+print(numero)
+
+#range= repetir n veces.Se usa cuando no tenes lista.
+
+#len= saber tamano. Sirve para saber la cantidad de elemntos.
+
+len([1,3,4]) #resultado 3 elementos.
+
+#enumerate= toma el indice y trae el valor. Sirve por ej para generar un ranking.listado con el indice.
+
+for i,n in enumerate([10,6,7]):
+    print(i,n)
+
+#zip: une listas. une datos entre si.
+
+for nombre,edad in zip(["agustin","roberto",'carlos'],[48,101,95]):
+    print(nombre,edad)
+
+#max, min y sum=
+
+sum([20,40,30])# salida 90.
+max([40,55,90])#salida 90
+min([90,10,5])#salida 5.
+
+# quiero saber un numero final= sum,contador.
+# quiero armar una nueva lista= append.
+#quiero solo algunos datos= condicion uso if.
+#quiero que me muestre el indice= enumerate.
+# tengo varias listas quiero combinarlas=zip
+
+celsius=[0,10,28,12]
+farenheit=[]
+
+for c in celsius:
+    f=c*9 / 5 + 32
+    farenheit.append(f)
+    
+
+print(farenheit)
+
+productos=['buzo','zapatilla','medias']
+precios=[20,45,10]
+
+for producto,precio in zip(productos,precios):
+    print(producto,precio)
+
+#Contar cuantas veces aparece un dato.
+
+datos=[10,3,2,1,2,50,10,2,1]
+repeticion={}
+
+for dato in datos:
+    if dato in repeticion: 
+        repeticion[dato]+= 1
+    else:repeticion[dato]= 1
+
+print(repeticion)
+
+
+# Analisis de Ventas
+#1- Venta total. contador o sum.
+#2- ventas mayores a 15 dolares. if condicion.
+#3- producto mas vendido. max.
+#4- cantidad de productos vendidos.
+
+productos=['pan','leche','gelatina','huevos','leche','huevos','pan','huevos']
+
+precios=[10,20,40,70,20,70,10,70]
+
+total_ventas=0
+ventas_altas=[]
+repeticion_productos={}
+
+for producto,precio in zip(productos,precios):
+    total_ventas= total_ventas + precio
+
+    if precio > 15:
+        ventas_altas.append((producto,precio))
+    
+    if producto in repeticion_productos:
+        repeticion_productos[producto] += 1
+    else: repeticion_productos[producto] = 1
+
+    mas_vendido=""
+    max_cantidad=0
+
+    for producto in repeticion_productos:
+        if repeticion_productos[producto]>max_cantidad:
+            max_cantidad=repeticion_productos[producto]
+            mas_vendido=producto
+
+print('Total de ventas:',total_ventas)
+print('Ventas mas altas ',ventas_altas)
+print('Producto que mas se repite: ',repeticion_productos)
+print('Producto mas vendido: ',mas_vendido)
